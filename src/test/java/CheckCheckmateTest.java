894c652eacf085d7525b4e6f570d828989b4a280
agrawl10 <agrawl10@illinois.edu>
2018-09-19 18:17:49 -0500

    Started work on 1.1

--- src/test/java/CheckCheckmateTest.java
+++ src/test/java/CheckCheckmateTest.java
@@ -34,11 +34,21 @@ public class CheckCheckmateTest extends TestCase {
         Piece pawnOne = board.getPieceAtPosition(5,1);
         gameInstance.movePiece(pawnOne, Coordinate.getCoordinate(5,3) );
 
+        assertFalse(gameInstance.isCheckmated(players.get(0)));
+        assertFalse(gameInstance.isStalemated(players.get(0)));
+
+
         Piece pawnTwo = board.getPieceAtPosition(4,6);
         gameInstance.movePiece(pawnTwo, Coordinate.getCoordinate(4,4) );
 
+        assertFalse(gameInstance.isCheckmated(players.get(0)));
+        assertFalse(gameInstance.isStalemated(players.get(0)));
+
         Piece pawnThree = board.getPieceAtPosition(6,1);
         gameInstance.movePiece(pawnThree, Coordinate.getCoordinate(6,3) );
+
+        assertFalse(gameInstance.isCheckmated(players.get(0)));
+        assertFalse(gameInstance.isStalemated(players.get(0)));
 
         Piece queenOne = board.getPieceAtPosition(3, 7);
         gameInstance.movePiece(queenOne, Coordinate.getCoordinate(7,3) );
@@ -48,27 +58,52 @@ public class CheckCheckmateTest extends TestCase {
         assertFalse(gameInstance.isChecked(players.get(1)));
 
         assertTrue(gameInstance.isCheckmated(players.get(0)));
+        assertFalse(gameInstance.isStalemated(players.get(0)));
         System.out.println(board);
     }
 
     public void testScholarMate(){
         Piece pawnOne = board.getPieceAtPosition(4, 1);
         gameInstance.movePiece(pawnOne, Coordinate.getCoordinate(4,3) );
+        assertFalse(gameInstance.isCheckmated(players.get(1)));
+        assertFalse(gameInstance.isStalemated(players.get(1)));
+        assertFalse(gameInstance.isCheckmated(players.get(0)));
+        assertFalse(gameInstance.isStalemated(players.get(0)));
 
         Piece pawnTwo = board.getPieceAtPosition(4,6);
         gameInstance.movePiece(pawnTwo, Coordinate.getCoordinate(4,4) );
+        assertFalse(gameInstance.isCheckmated(players.get(1)));
+        assertFalse(gameInstance.isStalemated(players.get(1)));
+        assertFalse(gameInstance.isCheckmated(players.get(0)));
+        assertFalse(gameInstance.isStalemated(players.get(0)));
 
         Piece bishopOne = board.getPieceAtPosition(5,0);
         gameInstance.movePiece(bishopOne, Coordinate.getCoordinate(2,3));
+        assertFalse(gameInstance.isCheckmated(players.get(1)));
+        assertFalse(gameInstance.isStalemated(players.get(1)));
+        assertFalse(gameInstance.isCheckmated(players.get(0)));
+        assertFalse(gameInstance.isStalemated(players.get(0)));
 
         Piece knightOne = board.getPieceAtPosition(1,7);
         gameInstance.movePiece(knightOne, Coordinate.getCoordinate(2,5));
+        assertFalse(gameInstance.isCheckmated(players.get(1)));
+        assertFalse(gameInstance.isStalemated(players.get(1)));
+        assertFalse(gameInstance.isCheckmated(players.get(0)));
+        assertFalse(gameInstance.isStalemated(players.get(0)));
 
         Piece queenOne = board.getPieceAtPosition(3, 0);
         gameInstance.movePiece(queenOne, Coordinate.getCoordinate(5,2));
+        assertFalse(gameInstance.isCheckmated(players.get(1)));
+        assertFalse(gameInstance.isStalemated(players.get(1)));
+        assertFalse(gameInstance.isCheckmated(players.get(0)));
+        assertFalse(gameInstance.isStalemated(players.get(0)));
 
         Piece bishopTwo = board.getPieceAtPosition(5,7);
         gameInstance.movePiece(bishopTwo, Coordinate.getCoordinate(2,4));
+        assertFalse(gameInstance.isCheckmated(players.get(1)));
+        assertFalse(gameInstance.isStalemated(players.get(1)));
+        assertFalse(gameInstance.isCheckmated(players.get(0)));
+        assertFalse(gameInstance.isStalemated(players.get(0)));
 
         gameInstance.movePiece(queenOne, Coordinate.getCoordinate(5,6));
 
@@ -77,6 +112,7 @@ public class CheckCheckmateTest extends TestCase {
         assertTrue(gameInstance.isChecked(players.get(1)));
 
         assertTrue(gameInstance.isCheckmated(players.get(1)));
+        assertFalse(gameInstance.isStalemated(players.get(1)));
         System.out.println(board);
     }
 
@@ -86,31 +122,71 @@ public class CheckCheckmateTest extends TestCase {
 
         Piece pawnOne = board.getPieceAtPosition(4, 1);
         gameInstance.movePiece(pawnOne, Coordinate.getCoordinate(4,3) );
+        assertFalse(gameInstance.isCheckmated(players.get(1)));
+        assertFalse(gameInstance.isStalemated(players.get(1)));
+        assertFalse(gameInstance.isCheckmated(players.get(0)));
+        assertFalse(gameInstance.isStalemated(players.get(0)));
 
         Piece pawnTwo = board.getPieceAtPosition(2,6);
         gameInstance.movePiece(pawnTwo, Coordinate.getCoordinate(2,5) );
+        assertFalse(gameInstance.isCheckmated(players.get(1)));
+        assertFalse(gameInstance.isStalemated(players.get(1)));
+        assertFalse(gameInstance.isCheckmated(players.get(0)));
+        assertFalse(gameInstance.isStalemated(players.get(0)));
 
         Piece pawnThree = board.getPieceAtPosition(3,1);
         gameInstance.movePiece(pawnThree, Coordinate.getCoordinate(3,3) );
+        assertFalse(gameInstance.isCheckmated(players.get(1)));
+        assertFalse(gameInstance.isStalemated(players.get(1)));
+        assertFalse(gameInstance.isCheckmated(players.get(0)));
+        assertFalse(gameInstance.isStalemated(players.get(0)));
 
         Piece pawnFour = board.getPieceAtPosition(3,6);
         gameInstance.movePiece(pawnFour, Coordinate.getCoordinate(3,4) );
+        assertFalse(gameInstance.isCheckmated(players.get(1)));
+        assertFalse(gameInstance.isStalemated(players.get(1)));
+        assertFalse(gameInstance.isCheckmated(players.get(0)));
+        assertFalse(gameInstance.isStalemated(players.get(0)));
 
         Piece knightOne = board.getPieceAtPosition(1,0);
         gameInstance.movePiece(knightOne, Coordinate.getCoordinate(2,2) );
+        assertFalse(gameInstance.isCheckmated(players.get(1)));
+        assertFalse(gameInstance.isStalemated(players.get(1)));
+        assertFalse(gameInstance.isCheckmated(players.get(0)));
+        assertFalse(gameInstance.isStalemated(players.get(0)));
 
         gameInstance.movePiece(pawnFour, pawnOne.getPosition());
+        assertFalse(gameInstance.isCheckmated(players.get(1)));
+        assertFalse(gameInstance.isStalemated(players.get(1)));
+        assertFalse(gameInstance.isCheckmated(players.get(0)));
+        assertFalse(gameInstance.isStalemated(players.get(0)));
 
         gameInstance.movePiece(knightOne, pawnFour.getPosition());
+        assertFalse(gameInstance.isCheckmated(players.get(1)));
+        assertFalse(gameInstance.isStalemated(players.get(1)));
+        assertFalse(gameInstance.isCheckmated(players.get(0)));
+        assertFalse(gameInstance.isStalemated(players.get(0)));
 
         Piece knightTwo = board.getPieceAtPosition(1,7);
         gameInstance.movePiece(knightTwo, Coordinate.getCoordinate(3,6));
+        assertFalse(gameInstance.isCheckmated(players.get(1)));
+        assertFalse(gameInstance.isStalemated(players.get(1)));
+        assertFalse(gameInstance.isCheckmated(players.get(0)));
+        assertFalse(gameInstance.isStalemated(players.get(0)));
 
         Piece queenOne = board.getPieceAtPosition(3,0);
         gameInstance.movePiece(queenOne, Coordinate.getCoordinate(4,1));
+        assertFalse(gameInstance.isCheckmated(players.get(1)));
+        assertFalse(gameInstance.isStalemated(players.get(1)));
+        assertFalse(gameInstance.isCheckmated(players.get(0)));
+        assertFalse(gameInstance.isStalemated(players.get(0)));
 
         Piece knightThree = board.getPieceAtPosition(6,7);
         gameInstance.movePiece(knightThree, Coordinate.getCoordinate(5,5));
+        assertFalse(gameInstance.isCheckmated(players.get(1)));
+        assertFalse(gameInstance.isStalemated(players.get(1)));
+        assertFalse(gameInstance.isCheckmated(players.get(0)));
+        assertFalse(gameInstance.isStalemated(players.get(0)));
 
         gameInstance.movePiece(knightOne, Coordinate.getCoordinate(3,5));
 
--- src/test/java/StalemateTest.java
+++ src/test/java/StalemateTest.java
@@ -24,50 +24,126 @@ public class StalemateTest extends TestCase {
     {
         Piece pawnOneWhite = board.getPieceAtPosition(4,1);
         gameInstance.movePiece(pawnOneWhite, Coordinate.getCoordinate(4,2) );
+        assertFalse(gameInstance.isCheckmated(players.get(1)));
+        assertFalse(gameInstance.isStalemated(players.get(1)));
+        assertFalse(gameInstance.isCheckmated(players.get(0)));
+        assertFalse(gameInstance.isStalemated(players.get(0)));
 
         Piece pawnOneBlack = board.getPieceAtPosition(0,6);
         gameInstance.movePiece(pawnOneBlack, Coordinate.getCoordinate(0,4) );
+        assertFalse(gameInstance.isCheckmated(players.get(1)));
+        assertFalse(gameInstance.isStalemated(players.get(1)));
+        assertFalse(gameInstance.isCheckmated(players.get(0)));
+        assertFalse(gameInstance.isStalemated(players.get(0)));
 
         Piece whiteQueen = board.getPieceAtPosition(3,0);
         gameInstance.movePiece(whiteQueen, Coordinate.getCoordinate(7,4));
+        assertFalse(gameInstance.isCheckmated(players.get(1)));
+        assertFalse(gameInstance.isStalemated(players.get(1)));
+        assertFalse(gameInstance.isCheckmated(players.get(0)));
+        assertFalse(gameInstance.isStalemated(players.get(0)));
 
         Piece rookOneBlack = board.getPieceAtPosition(0,7);
         gameInstance.movePiece(rookOneBlack, Coordinate.getCoordinate(0,5));
+        assertFalse(gameInstance.isCheckmated(players.get(1)));
+        assertFalse(gameInstance.isStalemated(players.get(1)));
+        assertFalse(gameInstance.isCheckmated(players.get(0)));
+        assertFalse(gameInstance.isStalemated(players.get(0)));
 
         gameInstance.movePiece(whiteQueen, pawnOneBlack.getPosition());
+        assertFalse(gameInstance.isCheckmated(players.get(1)));
+        assertFalse(gameInstance.isStalemated(players.get(1)));
+        assertFalse(gameInstance.isCheckmated(players.get(0)));
+        assertFalse(gameInstance.isStalemated(players.get(0)));
 
         Piece pawnTwoBlack = board.getPieceAtPosition(7,6);
         gameInstance.movePiece(pawnTwoBlack, Coordinate.getCoordinate(7,4) );
+        assertFalse(gameInstance.isCheckmated(players.get(1)));
+        assertFalse(gameInstance.isStalemated(players.get(1)));
+        assertFalse(gameInstance.isCheckmated(players.get(0)));
+        assertFalse(gameInstance.isStalemated(players.get(0)));
 
         Piece pawnTwoWhite = board.getPieceAtPosition(7,1);
         gameInstance.movePiece(pawnTwoWhite, Coordinate.getCoordinate(7,3) );
+        assertFalse(gameInstance.isCheckmated(players.get(1)));
+        assertFalse(gameInstance.isStalemated(players.get(1)));
+        assertFalse(gameInstance.isCheckmated(players.get(0)));
+        assertFalse(gameInstance.isStalemated(players.get(0)));
 
         gameInstance.movePiece(rookOneBlack, Coordinate.getCoordinate(7,5));
+        assertFalse(gameInstance.isCheckmated(players.get(1)));
+        assertFalse(gameInstance.isStalemated(players.get(1)));
+        assertFalse(gameInstance.isCheckmated(players.get(0)));
+        assertFalse(gameInstance.isStalemated(players.get(0)));
 
         gameInstance.movePiece(whiteQueen, Coordinate.getCoordinate(2,6));
+        assertFalse(gameInstance.isCheckmated(players.get(1)));
+        assertFalse(gameInstance.isStalemated(players.get(1)));
+        assertFalse(gameInstance.isCheckmated(players.get(0)));
+        assertFalse(gameInstance.isStalemated(players.get(0)));
 
         Piece pawnThreeBlack = board.getPieceAtPosition(5,6);
         gameInstance.movePiece(pawnThreeBlack, Coordinate.getCoordinate(5,5));
+        assertFalse(gameInstance.isCheckmated(players.get(1)));
+        assertFalse(gameInstance.isStalemated(players.get(1)));
+        assertFalse(gameInstance.isCheckmated(players.get(0)));
+        assertFalse(gameInstance.isStalemated(players.get(0)));
 
         gameInstance.movePiece(whiteQueen, Coordinate.getCoordinate(3,6));
+        assertFalse(gameInstance.isCheckmated(players.get(1)));
+        assertFalse(gameInstance.isStalemated(players.get(1)));
+        assertFalse(gameInstance.isCheckmated(players.get(0)));
+        assertFalse(gameInstance.isStalemated(players.get(0)));
 
         assertTrue(gameInstance.isChecked(players.get(1)));
+        assertFalse(gameInstance.isCheckmated(players.get(1)));
+        assertFalse(gameInstance.isStalemated(players.get(1)));
+        assertFalse(gameInstance.isCheckmated(players.get(0)));
+        assertFalse(gameInstance.isStalemated(players.get(0)));
 
         Piece blackKing = board.getPieceAtPosition(4,7);
         gameInstance.movePiece(blackKing, Coordinate.getCoordinate(5,6));
+        assertFalse(gameInstance.isCheckmated(players.get(1)));
+        assertFalse(gameInstance.isStalemated(players.get(1)));
+        assertFalse(gameInstance.isCheckmated(players.get(0)));
+        assertFalse(gameInstance.isStalemated(players.get(0)));
 
         gameInstance.movePiece(whiteQueen, Coordinate.getCoordinate(1,6));
+        assertFalse(gameInstance.isCheckmated(players.get(1)));
+        assertFalse(gameInstance.isStalemated(players.get(1)));
+        assertFalse(gameInstance.isCheckmated(players.get(0)));
+        assertFalse(gameInstance.isStalemated(players.get(0)));
 
         Piece blackQueen = board.getPieceAtPosition(3,7);
         gameInstance.movePiece(blackQueen, Coordinate.getCoordinate(3,2));
+        assertFalse(gameInstance.isCheckmated(players.get(1)));
+        assertFalse(gameInstance.isStalemated(players.get(1)));
+        assertFalse(gameInstance.isCheckmated(players.get(0)));
+        assertFalse(gameInstance.isStalemated(players.get(0)));
 
         gameInstance.movePiece(whiteQueen, Coordinate.getCoordinate(1,7));
+        assertFalse(gameInstance.isCheckmated(players.get(1)));
+        assertFalse(gameInstance.isStalemated(players.get(1)));
+        assertFalse(gameInstance.isCheckmated(players.get(0)));
+        assertFalse(gameInstance.isStalemated(players.get(0)));
 
         gameInstance.movePiece(blackQueen, Coordinate.getCoordinate(7,6));
+        assertFalse(gameInstance.isCheckmated(players.get(1)));
+        assertFalse(gameInstance.isStalemated(players.get(1)));
+        assertFalse(gameInstance.isCheckmated(players.get(0)));
+        assertFalse(gameInstance.isStalemated(players.get(0)));
 
         gameInstance.movePiece(whiteQueen, Coordinate.getCoordinate(2,7));
+        assertFalse(gameInstance.isCheckmated(players.get(1)));
+        assertFalse(gameInstance.isStalemated(players.get(1)));
+        assertFalse(gameInstance.isCheckmated(players.get(0)));
+        assertFalse(gameInstance.isStalemated(players.get(0)));
 
         gameInstance.movePiece(blackKing, Coordinate.getCoordinate(6,5));
+        assertFalse(gameInstance.isCheckmated(players.get(1)));
+        assertFalse(gameInstance.isStalemated(players.get(1)));
+        assertFalse(gameInstance.isCheckmated(players.get(0)));
+        assertFalse(gameInstance.isStalemated(players.get(0)));
 
         gameInstance.movePiece(whiteQueen, Coordinate.getCoordinate(4,5));
 
